795a2c5667eb16eb1045e1b16e9305c9811d6802
Kemcy Dalleygrand <kemcy.dalleygrand@outlook.com>
2023-01-13 16:36:04 -0500

    adds link to navs

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,6 +1,19 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+
 import { Link } from "react-router-dom"
 const Nav = () => {
-    return <h1>Navigation</h1>
+    return (
+        <div className="nav">
+            <ul>
+                <li className="nav-item">
+                    <Link to={'/'}>Home</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to={'/about'} >About</Link>
+                </li>
+            </ul>
+        </div>
+    )
 }
 
 export default Nav
