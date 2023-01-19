1596755f6d90a33a19d62f1742be2fc6ba91e10e
Kemcy Dalleygrand <kemcy.dalleygrand@outlook.com>
2023-01-19 14:41:16 -0500

    pico css minimal styling

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -3,11 +3,13 @@
 import { Link } from "react-router-dom"
 const Navigation = () => {
     return (
-        <>
-        <h1>Navigations</h1>
-        <Link to={'/home'}>Home</Link>
-        <Link to={'/about'}>Home</Link>
-        </>
+        <nav>
+            <ul>
+            <Link to={'/'}>Home</Link>
+            <Link to={'/about'}>About</Link>
+            </ul>
+        {/* <h1>Navigations</h1> */}
+        </nav>
     )
 }
 
