3c2d2bf976199bc4cfdb5cf7a073c1f8c91f765b
Kemcy Dalleygrand <kemd91@gmail.com>
2023-03-10 10:56:53 -0500

    wip: -u show

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ function App() {
           </>
         )}/>
         <Route path="/about" element={<About />} />
-        {/* <Route path='/:id' element={<Show allGames={games} games={ currentGames } />} /> */}
+        <Route path='/:id' element={<Show allGames={games} games={ currentGames } />} />
 
       </Routes>
 
